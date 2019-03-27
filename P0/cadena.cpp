4b1c7ad3c1f2a0964484c1af2e15f2cd56f71923
BorjaRomeroFernandez <1415.romero.borja@gmail.com>
2019-03-27 10:40:08 +0100

    Added methods to cadena.[hc]pp

--- P0/cadena.cpp
+++ P0/cadena.cpp
@@ -1,1 +1,138 @@
+#include <cstring>
+#include <iostream>
 #include "cadena.hpp"
+
+Cadena::Cadena(unsigned tamano, char caracter) : s_(new char[tamano + 1]),
+                                                 tam_(tamano)
+{
+    for (unsigned i = 0; i < tamano; ++i)
+        this->s_[i] = caracter;
+
+    this->s_[tamano] = '\0';
+}
+
+Cadena::Cadena(const Cadena &C) : s_(new char[C.tam_ + 1]),
+                                  tam_(C.length())
+{
+    strcpy(this->s_, C.s_);
+}
+
+Cadena::Cadena(const char *cadena) : s_(new char[strlen(cadena) + 1]),
+                                     tam_(strlen(cadena))
+{
+    strcpy(this->s_, cadena);
+
+    this->s_[this->tam_] = '\0';
+}
+
+Cadena Cadena::operator=(const Cadena &C)
+{
+    if (this->length() == C.length())
+    {
+        strcpy(this->s_, C.s_);
+    }
+    else
+    {
+        delete[] this->s_;
+        this->tam_ = C.length();
+        this->s_ = new char[this->tam_ + 1];
+        strcpy(this->s_, C.s_);
+    }
+}
+
+Cadena Cadena::operator=(const char *cadena)
+{
+    if (this->tam_ == strlen(cadena))
+    {
+        strcpy(this->s_, cadena);
+    }
+    else
+    {
+        delete[] this->s_;
+        this->tam_ = strlen(cadena);
+        this->s_ = new char[this->tam_ + 1];
+        strcpy(this->s_, cadena);
+    }
+
+    this->s_[this->tam_] = '\0';
+}
+
+Cadena::operator const char *()
+{
+    std::cout << this->s_ << std::endl;
+}
+
+Cadena Cadena::operator+=(const Cadena &C)
+{
+    char *aux = new char[this->tam_];
+    int i = 0;
+
+    while (i < this->tam_)
+    {
+        aux[i] = this->s_[i];
+        ++i;
+    }
+
+    strcat(aux, C.s_);
+
+    delete[] this->s_;
+    this->tam_ += C.length();
+    this->s_ = new char[this->length() + 1];
+    strcpy(this->s_, aux);
+}
+
+Cadena Cadena::operator+(const Cadena &C)
+{
+    Cadena t = *this;
+
+    return t += C;
+}
+
+char Cadena::operator[](int n)
+{
+    return this->s_[n];
+}
+
+const char Cadena::operator[](int n) const
+{
+    return this->s_[n];
+}
+
+char Cadena::at(int n)
+{
+    if (n < 0 || n > this->tam_ - 1)
+        throw std::out_of_range("El índice es inválido");
+    else
+        return this->s_[n];
+}
+
+const char Cadena::at(int n) const
+{
+    if (n < 0 || n > this->tam_ - 1)
+        throw std::out_of_range("El índice es inválido");
+    else
+        return this->s_[n];
+}
+
+Cadena Cadena::substr(unsigned indice, unsigned tamano)
+{
+    if(indice > this->tam_ - 1 || (indice + tamano) > this->tam_ - 1)
+        throw std::out_of_range("El índice es inválido");
+
+    char* aux = new char[tamano + 1];
+    unsigned i = indice;
+
+    while(i < indice + tamano)
+    {
+        aux[i] = this->s_[i];
+    }
+
+    aux[indice + tamano] = '\0';
+
+    return *this;
+}
+
+Cadena::~Cadena()
+{
+    
+}
--- P0/cadena.hpp
+++ P0/cadena.hpp
@@ -3,8 +3,37 @@
 
 class Cadena
 {
-  public:
-  private:
+public:
+  Cadena(unsigned t = 0, char c = ' ');
+  Cadena(const Cadena &C);
+  Cadena(const char *cadena);
+
+  Cadena operator=(const Cadena &C);
+  Cadena operator=(const char *cadena);
+  operator const char *();
+  Cadena operator+=(const Cadena &C);
+  Cadena operator+(const Cadena &C);
+  char operator[](int n);
+  const char operator[](int n) const;
+
+  char at(int n);
+  const char at(int n) const;
+  Cadena substr(unsigned indice, unsigned tamano);
+
+  const unsigned length() const { return tam_; }
+
+  ~Cadena();
+
+private:
+  char *s_;
+  unsigned tam_;
 };
+
+bool operator==(const Cadena &C1, const Cadena &C2);
+bool operator!=(const Cadena &C1, const Cadena &C2);
+bool operator>(const Cadena &C1, const Cadena &C2);
+bool operator<(const Cadena &C1, const Cadena &C2);
+bool operator>=(const Cadena &C1, const Cadena &C2);
+bool operator<=(const Cadena &C1, const Cadena &C2);
 
 #endif
