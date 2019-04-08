f4a77087a7991c157bd7599ec5cd72716e635742
BorjaRomeroFernandez <1415.romero.borja@gmail.com>
2019-04-08 09:55:49 +0200

    Fixed all errors in fecha.[hc]pp :heavy_check_mark:

--- P0/fecha.cpp
+++ P0/fecha.cpp
@@ -1,6 +1,6 @@
 #include <ctime>
 #include <cstdio>
-#include <iostream>
+#include <cstring>
 #include "fecha.hpp"
 
 Fecha::Fecha(int d, int m, int a) : dia_(d), mes_(m), anno_(a)
@@ -25,7 +25,8 @@ Fecha::Fecha(const char *fecha)
     std::time_t tiempo_actual = std::time(nullptr);
     std::tm *fecha_actual = std::localtime(&tiempo_actual);
 
-    sscanf(fecha, "%02d/%02d/%4d", &dia_, &mes_, &anno_);
+    if (sscanf(fecha, "%02d/%02d/%4d", &dia_, &mes_, &anno_) != 3)
+        throw Fecha::Invalida("Formato incorrecto");
 
     if (dia_ == 0)
         dia_ = fecha_actual->tm_mday;
@@ -39,7 +40,7 @@ Fecha::Fecha(const char *fecha)
     valida();
 }
 
-void Fecha::valida()
+void Fecha::valida() const
 {
     if (anno_ < Fecha::AnnoMinimo || anno_ > Fecha::AnnoMaximo)
         throw Fecha::Invalida("El año es inválido");
@@ -62,23 +63,27 @@ void Fecha::valida()
         else
         {
             if (anno_ % 4 == 0 && (anno_ % 400 == 0 || anno_ % 100 != 0))
+            {
                 if (dia_ < 1 || dia_ > 29)
-                    throw Fecha::Invalida("El día es inválido: el mes tiene 29 días");
-                else
                 {
-                    if (dia_ < 1 || dia_ > 28)
-                        throw Fecha::Invalida("El día es inválido: el mes tiene 28 días");
+                    throw Fecha::Invalida("El día es inválido: el mes tiene 29 días");
                 }
+            }
+            else
+            {
+                if (dia_ < 1 || dia_ > 28)
+                    throw Fecha::Invalida("El día es inválido: el mes tiene 28 días");
+            }
         }
     }
 }
 
 Fecha &Fecha::operator++()
 {
-    return (*this += 1);
+    return *this += 1;
 }
 
-Fecha &Fecha::operator++(int)
+Fecha Fecha::operator++(int)
 {
     Fecha t = *this;
     *this += 1;
@@ -88,10 +93,10 @@ Fecha &Fecha::operator++(int)
 
 Fecha &Fecha::operator--()
 {
-    return (*this += -1);
+    return *this += -1;
 }
 
-Fecha &Fecha::operator--(int)
+Fecha Fecha::operator--(int)
 {
     Fecha t = *this;
     *this += -1;
@@ -99,14 +104,14 @@ Fecha &Fecha::operator--(int)
     return t;
 }
 
-Fecha &Fecha::operator+(int n)
+Fecha &Fecha::operator+(int n) const
 {
     Fecha t = *this;
 
     return t += n;
 }
 
-Fecha &Fecha::operator-(int n)
+Fecha &Fecha::operator-(int n) const
 {
     Fecha t = *this;
 
@@ -120,129 +125,54 @@ Fecha &Fecha::operator-=(int n)
 
 Fecha &Fecha::operator+=(int n)
 {
-    this->dia_ += n;
+    if (n != 0)
+    {
+        std::tm *time_tm = new std::tm{0, 0, 0, (dia_ + n), (mes_ - 1), (anno_ - 1900), 0, 0, 0, 0, 0};
 
-    tm *fecha;
+        std::mktime(time_tm);
 
-    fecha->tm_mday = this->dia_;
-    fecha->tm_mon = this->mes_;
-    fecha->tm_year = this->anno_;
+        dia_ = time_tm->tm_mday;
+        mes_ = time_tm->tm_mon + 1;
+        anno_ = time_tm->tm_year + 1900;
 
-    mktime(fecha);
-
-    this->dia_ = fecha->tm_mday;
-    this->mes_ = fecha->tm_mon;
-    this->anno_ = fecha->tm_year;
+        valida();
+    }
 
     return *this;
 }
 
-Fecha &Fecha::operator=(const Fecha &F)
+Fecha::operator const char *() const
 {
-    this->dia_ = F.dia();
-    this->mes_ = F.mes();
-    this->anno_ = F.anno();
+    char *cadena = new char[40];
 
-    return *this;
+    std::tm *fecha = new std::tm{0, 0, 0, dia_, (mes_ - 1), (anno_ - 1900), 0, 0, 0, 0, 0};
+
+    std::mktime(fecha);
+
+    char *DIASEM[] = {"domingo", "lunes", "martes", "miércoles", "jueves", "viernes", "sábado"};
+    char *MES[] = {"enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"};
+
+    sprintf(cadena, "%s %d de %s de %d", DIASEM[fecha->tm_wday], fecha->tm_mday, MES[fecha->tm_mon], fecha->tm_year + 1900);
+
+    return (const char *)cadena;
 }
 
-Fecha::operator const char *()
-{
-    tm *fecha;
-
-    fecha->tm_mday = this->dia_;
-    fecha->tm_mon = this->mes_;
-    fecha->tm_year = this->anno_;
-
-    mktime(fecha);
-
-    const char *DIASEM;
-    const char *MES;
-
-    switch (fecha->tm_wday)
-    {
-    case 0:
-        DIASEM = "lunes";
-        break;
-    case 1:
-        DIASEM = "martes";
-        break;
-    case 2:
-        DIASEM = "miércoles";
-        break;
-    case 3:
-        DIASEM = "jueves";
-        break;
-    case 4:
-        DIASEM = "viernes";
-        break;
-    case 5:
-        DIASEM = "sábado";
-        break;
-    case 6:
-        DIASEM = "domingo";
-        break;
-    }
-
-    switch (fecha->tm_mon)
-    {
-    case 0:
-        MES = "enero";
-        break;
-    case 1:
-        MES = "debrero";
-        break;
-    case 2:
-        MES = "marzo";
-        break;
-    case 3:
-        MES = "abril";
-        break;
-    case 4:
-        MES = "mayo";
-        break;
-    case 5:
-        MES = "junio";
-        break;
-    case 6:
-        MES = "julio";
-        break;
-    case 7:
-        MES = "agosto";
-        break;
-    case 8:
-        MES = "septiembre";
-        break;
-    case 9:
-        MES = "octubre";
-        break;
-    case 10:
-        MES = "noviembre";
-        break;
-    case 11:
-        MES = "diciembre";
-        break;
-    }
-
-    std::cout << DIASEM << " " << fecha->tm_mday << " de " << MES << " de " << fecha->tm_year;
-}
-
-bool operator==(const Fecha &F1, const Fecha &F2)
+bool operator==(const Fecha &F1, const Fecha &F2) noexcept
 {
     return (F1.dia() == F2.dia() && F1.mes() == F2.mes() && F1.anno() == F2.anno()) ? true : false;
 }
 
-bool operator!=(const Fecha &F1, const Fecha &F2)
+bool operator!=(const Fecha &F1, const Fecha &F2) noexcept
 {
     return !(F1 == F2);
 }
 
-bool operator>(const Fecha &F1, const Fecha &F2)
+bool operator>(const Fecha &F1, const Fecha &F2) noexcept
 {
     return F2 < F1;
 }
 
-bool operator<(const Fecha &F1, const Fecha &F2)
+bool operator<(const Fecha &F1, const Fecha &F2) noexcept
 {
     if (F1.anno() < F2.anno())
     {
@@ -272,14 +202,16 @@ bool operator<(const Fecha &F1, const Fecha &F2)
             }
         }
     }
+
+    return false;
 }
 
-bool operator>=(const Fecha &F1, const Fecha &F2)
+bool operator>=(const Fecha &F1, const Fecha &F2) noexcept
 {
     return !(F1 < F2);
 }
 
-bool operator<=(const Fecha &F1, const Fecha &F2)
+bool operator<=(const Fecha &F1, const Fecha &F2) noexcept
 {
     return !(F2 < F1);
 }
