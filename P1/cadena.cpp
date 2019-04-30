1bdac2b7f78df4e1b1c1d7b9ba8ef7a952dc1375
BorjaRomeroFernandez <1415.romero.borja@gmail.com>
2019-04-30 10:17:35 +0200

    Fixed all cadena.[hc]pp errors :heavy_check_mark:

--- P1/cadena.cpp
+++ P1/cadena.cpp
@@ -26,6 +26,12 @@ Cadena::Cadena(const char *cadena) : s_(new char[strlen(cadena) + 1]),
     this->s_[this->tam_] = '\0';
 }
 
+Cadena::Cadena(Cadena &&C) : s_(C.s_), tam_(C.tam_)
+{
+    C.s_ = nullptr;
+    C.tam_ = 0;
+}
+
 Cadena &Cadena::operator=(const Cadena &C) noexcept
 {
     if (this->length() == C.length())
@@ -62,9 +68,16 @@ Cadena &Cadena::operator=(const char *cadena) noexcept
     return *this;
 }
 
-Cadena::operator const char *() const noexcept
+Cadena &Cadena::operator=(Cadena &&C) noexcept
 {
-    return this->s_;
+
+    s_ = C.s_;
+    tam_ = C.tam_;
+
+    C.s_ = nullptr;
+    C.tam_ = 0;
+
+    return *this;
 }
 
 Cadena &Cadena::operator+=(const Cadena &C) noexcept
@@ -107,6 +120,11 @@ const char &Cadena::operator[](int n) const noexcept
     return this->s_[n];
 }
 
+const char *Cadena::c_str() const
+{
+    return this->s_;
+}
+
 char &Cadena::at(unsigned n)
 {
     if (n < 0 || n > this->tam_ - 1)
@@ -115,7 +133,7 @@ char &Cadena::at(unsigned n)
         return this->s_[n];
 }
 
-const char Cadena::at(unsigned n) const
+char Cadena::at(unsigned n) const
 {
     if (n < 0 || n >= this->tam_)
         throw std::out_of_range("El índice es inválido");
@@ -150,7 +168,7 @@ Cadena::~Cadena()
 
 bool operator==(const Cadena &C1, const Cadena &C2) noexcept
 {
-    return (strcmp(C1, C2) == 0);
+    return (strcmp(C1.c_str(), C2.c_str()) == 0);
 }
 
 bool operator!=(const Cadena &C1, const Cadena &C2) noexcept
@@ -165,7 +183,7 @@ bool operator>(const Cadena &C1, const Cadena &C2) noexcept
 
 bool operator<(const Cadena &C1, const Cadena &C2) noexcept
 {
-    return (strcmp(C1, C2) < 0);
+    return (strcmp(C1.c_str(), C2.c_str()) < 0);
 }
 
 bool operator>=(const Cadena &C1, const Cadena &C2) noexcept
@@ -176,4 +194,39 @@ bool operator>=(const Cadena &C1, const Cadena &C2) noexcept
 bool operator<=(const Cadena &C1, const Cadena &C2) noexcept
 {
     return !(C2 < C1);
+}
+
+std::ostream &operator<<(std::ostream &os, const Cadena &C)
+{
+    os << C.c_str();
+
+    return os;
+}
+
+std::istream &operator>>(std::istream &is, Cadena &C)
+{
+
+    size_t i = 0;
+    char *s = new char[32];
+    char a;
+
+    while (isspace(is.get()) != 0 && i < 32)
+    {
+        i++;
+    }
+
+    is.unget();
+    i = 0;
+
+    while (!isspace(is.peek()) && i < 32 && is.good())
+    {
+        a = is.get();
+        s[i++] = a;
+    }
+
+    s[i] = '\0';
+    C = Cadena(s);
+    delete[] s;
+
+    return is;
 }
--- P1/cadena.hpp
+++ P1/cadena.hpp
@@ -1,26 +1,49 @@
 #ifndef CADENA_HPP_
 #define CADENA_HPP_
 
+#include <iterator>
+
 class Cadena
 {
 public:
   explicit Cadena(unsigned t = 0, char c = ' ');
   Cadena(const Cadena &C);
   Cadena(const char *cadena);
+  Cadena(Cadena &&C);
 
   Cadena &operator=(const Cadena &C) noexcept;
   Cadena &operator=(const char *cadena) noexcept;
-  operator const char *() const noexcept;
+  Cadena &operator=(Cadena &&C) noexcept;
   Cadena &operator+=(const Cadena &C) noexcept;
   Cadena operator+(const Cadena &C) const noexcept;
   char &operator[](int n) noexcept;
   const char &operator[](int n) const noexcept;
 
+  const char *c_str() const;
   char &at(unsigned n);
-  const char at(unsigned n) const;
+  char at(unsigned n) const;
   Cadena substr(unsigned indice, unsigned tamano) const;
 
   unsigned length() const noexcept { return tam_; }
+
+  typedef char *iterator;
+  typedef const char *const_iterator;
+  typedef std::reverse_iterator<iterator> reverse_iterator;
+  typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+
+  iterator begin() { return s_; }
+  const_iterator begin() const { return cbegin(); } // return s_
+  const_iterator cbegin() const { return s_; }
+  reverse_iterator rbegin() { return reverse_iterator(end()); }
+  const_reverse_iterator rbegin() const { return crbegin(); }
+  const_reverse_iterator crbegin() const { return const_reverse_iterator(end()); }
+
+  iterator end() { return s_ + tam_; }
+  const_iterator end() const { return cend(); } // return s_+ tam_
+  const_iterator cend() const { return s_ + tam_; }
+  reverse_iterator rend() { return reverse_iterator(begin()); }
+  const_reverse_iterator rend() const { return crend(); }
+  const_reverse_iterator crend() const { return const_reverse_iterator(begin()); }
 
   ~Cadena();
 
@@ -35,5 +58,8 @@ bool operator>(const Cadena &C1, const Cadena &C2) noexcept;
 bool operator<(const Cadena &C1, const Cadena &C2) noexcept;
 bool operator>=(const Cadena &C1, const Cadena &C2) noexcept;
 bool operator<=(const Cadena &C1, const Cadena &C2) noexcept;
+
+std::ostream &operator<<(std::ostream &os, const Cadena &C);
+std::istream &operator>>(std::istream &is, Cadena &C);
 
 #endif
